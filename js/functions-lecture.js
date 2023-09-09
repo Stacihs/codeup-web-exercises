409eca7b6ce2279337235013d8729e48faac35f1
Staci Holmes-Segarra <staci.holmessegarra@gmail.com>
2023-09-09 16:39:14 -0500

    Add function lecture htmland js files and add functions exercises files

---
+++ js/functions-lecture.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// FUNCTION DECLARATION
+function addTwoNumbers(numOne, numTwo) {
+    return numOne + numTwo;
+}
+
+// ARROW FUNCTIONS DON'T HOIST
+
+// FUNCTION EXPRESSION
+// Assigning the value of a function to a variable
+//Function Expressions are good for passing a function to another function, or returning a function from a function
