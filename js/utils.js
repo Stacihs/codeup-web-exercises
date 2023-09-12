30167a74d108723ab16fb085f56fb2619f74d4cd
Staci Holmes-Segarra <staci.holmessegarra@gmail.com>
2023-09-12 08:28:35 -0500

    Add conditionals and warmups files

---
+++ js/utils.js
@@ -0,0 +1,13 @@
+function evenOrOdd(number) {
+    if (!isNumber(number)) {
+        return "That is not a number.";
+    } else if (number % 2 === 0) {
+        return "even";
+    } else {
+        return "odd";
+    }
+}
+
+function isNumber(input) {
+    if (input )
+}
