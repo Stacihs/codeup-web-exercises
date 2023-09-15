2557232b98444edf699e49a34b07ffea9ce864ef
Staci Holmes-Segarra <staci.holmessegarra@gmail.com>
2023-09-14 20:42:42 -0500

    Add completed for loops exercises

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,1 +1,35 @@
 "use strict";
+
+// EXERCISE 1
+function showMultiplicationTable(number) {
+    for (let i = 1; i <= 10; i++) {
+        console.log(`${number} x ${i} = ${number * i}`);
+    }
+}
+
+// showMultiplicationTable(7);
+
+
+// EXERCISE 2
+
+// for (let i = 1; i <= 10; i++) {
+//     let randomNum = Math.floor(Math.random() * (200 - 20 + 1)) + 20;
+//     if (randomNum % 2 === 0) {
+//         console.log(`${randomNum} is even`);
+//     } else {
+//         console.log(`${randomNum} is odd`);
+//     }
+// }
+
+//EXERCISE 3
+//     for (let i = 1; i <= 9; i++) {
+//         let iString = i.toString();
+//         console.log(iString.repeat(i));
+//     }
+
+
+// EXERCISE 4
+//     for (let i = 105; i > 5; i - 5) {
+//         console.log(i -= 5);
+//     }
+
