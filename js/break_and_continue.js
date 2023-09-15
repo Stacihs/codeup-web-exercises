3b6bbecfa17ead9c0c31a968eecb28666b00543e
Staci Holmes-Segarra <staci.holmessegarra@gmail.com>
2023-09-14 22:20:53 -0500

    Add break and continue exercise

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,1 +1,26 @@
 "use strict";
+
+//BREAK AND CONTINUE
+
+// let userNumber = parseInt(prompt("Enter an odd number between 1 and 50"));
+//
+// console.log(`Number to skip is: ${userNumber}`);
+// console.log("");
+//
+// for (let i = 1; i < 50; i++) {
+//     if (userNumber < 1 || userNumber > 50 || userNumber % 2 === 0) {
+//         userNumber = parseInt(prompt("Enter an odd number between 1 and 50"));
+//         break;
+//     }
+//
+//     if (i % 2 === 0) {
+//         continue;
+//     }
+//
+//     if (i === userNumber) {
+//         console.log(`Yikes! Skipping number: ${userNumber}`);
+//         continue;
+//     }
+//
+//     console.log(`Here is an odd number: ${i}`);
+// }
