bed02314e78764308d1498f7d68700ef434f440a
Staci Holmes-Segarra <staci.holmessegarra@gmail.com>
2023-09-19 23:05:01 -0500

    Refactor to make a function

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,26 +1,30 @@
 "use strict";
-// TODO make it a function add comments
+// TODO Add comments
 //BREAK AND CONTINUE
+function pickANumber() {
+    // TODO FIX PROMPT
+    let userNumber = parseInt(prompt("Enter an odd number between 1 and 50"));
+    do {
+        parseInt(prompt("Enter an odd number between 1 and 50"));
+        if (userNumber < 1 || userNumber > 50 || userNumber % 2 === 0) {
+            break;
+        }
+    }
+    while (true);
 
-// let userNumber = parseInt(prompt("Enter an odd number between 1 and 50"));
-//
-// console.log(`Number to skip is: ${userNumber}`);
-// console.log("");
-// // // TODO fix prompt
-// for (let i = 1; i < 50; i++) {
-//     if (userNumber < 1 || userNumber > 50 || userNumber % 2 === 0) {
-//         userNumber = parseInt(prompt("Enter an odd number between 1 and 50"));
-//         break;
-//     }
-//
-//     if (i % 2 === 0) {
-//         continue;
-//     }
-//
-//     if (i === userNumber) {
-//         console.log(`Yikes! Skipping number: ${userNumber}`);
-//         continue;
-//     }
-//
-//     console.log(`Here is an odd number: ${i}`);
-// }
+    console.log(`Number to skip is: ${userNumber}`);
+    console.log("");
+    for (let i = 1; i < 50; i++) {
+        if (i % 2 === 0) {
+            continue;
+        }
+        if (i === userNumber) {
+            console.log(`Yikes! Skipping number: ${userNumber}`);
+            continue;
+        }
+        console.log(`Here is an odd number: ${i}`);
+    }
+    return userNumber;
+}
+
+console.log(pickANumber());
