33a278423a5b860725c6c6d2b1df175c7f839655
Staci Holmes-Segarra <staci.holmessegarra@gmail.com>
2023-09-20 10:59:02 -0500

    Correct typos

--- js/utils.js
+++ js/utils.js
@@ -6,17 +6,19 @@ function concat(input1, input2) {
     return input1.toString() + input2.toString();
 }
 
-function isNumber(input){
+function isNumber(input) {
     return (typeof input !== "string"
-    && typeof input !== "boolean"
-    && typeof input !== "object"
-    && typeof input !== "undefined"
-        && !isNaN(input))
+        && typeof input !== "boolean"
+        && typeof input !== "object"
+        && typeof input !== "undefined"
+        && !isNaN(input)
+    );
 }
 
-function isNumberOrNumericString(input){
-    return typeof input !== "boolean"
-    && typeof input !== "object"
-    && typeof input !== "undefined"
+function isNumberOrNumericString(input) {
+    return (typeof input !== "boolean"
+        && typeof input !== "object"
+        && typeof input !== "undefined"
         && isNaN(parseFloat(input))
+    );
 }
