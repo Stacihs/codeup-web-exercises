199fb612766505046362c428484a5ef1a7401e06
Staci Holmes-Segarra <staci.holmessegarra@gmail.com>
2023-10-10 22:36:30 -0500

    Changed and used the correct method, innerHTML, in the changeProfileName function

--- js/attributes-script.js
+++ js/attributes-script.js
@@ -4,13 +4,18 @@ const profileName = document.querySelector('#profile-name');
 const profileDesc = document.querySelector('#profile-desc');
 const profileCard = document.querySelector('#profile-card');
 
+const intervalPicId = setTimeout(changeProfilePic, 2000);
+const intervalNameId = setTimeout(changeProfileName, 4000);
+const timeOutID = setTimeout(changeDescription, 6000);
+const intervalCardId = setInterval(changeCardBackground, 2000);
+
 
 //FUNCTIONS
 function changeProfilePic () {
     profilePic.src = "https://images.pexels.com/photos/4668513/pexels-photo-4668513.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
 }
 function changeProfileName() {
-    profileName.innerText = "Jane Doe";
+    profileName.innerHTML = "Jane Doe";
 }
 
 function changeDescription() {
@@ -22,10 +27,7 @@ function changeCardBackground() {
 }
 
 
-const intervalPicId = setTimeout(changeProfilePic, 2000);
-const intervalNameId = setTimeout(changeProfileName, 4000);
-const timeOutID = setTimeout(changeDescription, 6000);
-const intervalCardId = setInterval(changeCardBackground, 2000);
+
 // use set timeout
 
 // Write code that toggles a class on the "profile-card" that changes its background color every two seconds. Use setInterval
