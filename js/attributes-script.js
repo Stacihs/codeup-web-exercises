aae38b8ddf4a1748f64bd8f2c38b663579290a58
Staci Holmes-Segarra <staci.holmessegarra@gmail.com>
2023-10-17 22:26:49 -0500

    Completed bonus on profileCard.html

--- js/attributes-script.js
+++ js/attributes-script.js
@@ -1,58 +1,55 @@
+(function() {
+ "use strict";
+
 // VARIABLES
-const profilePic = document.querySelector('#profile-pic');
-const profileName = document.querySelector('#profile-name');
-const profileDesc = document.querySelector('#profile-desc');
-const profileCard = document.querySelector('#profile-card');
+    const profilePic = document.querySelector('#profile-pic');
+    const profileName = document.querySelector('#profile-name');
+    const profileDesc = document.querySelector('#profile-desc');
+    const profileCard = document.querySelector('#profile-card');
 
-const colorsArr = ["pink", "red", "orange", "yellow", "gray"];
+    const colorsArr = ["pink", "red", "orange", "yellow", "gray"];
 
-const picDelay = 2000;
-const nameDelay = 4000;
-const descDelay = 6000;
-const cardDelay = 2000;
+    const picDelay = 2000;
+    const nameDelay = 4000;
+    const descDelay = 6000;
+    const cardDelay = 2000;
+    const nameChange = 7000;
 
 
 //FUNCTIONS
-function cardDOMManipulation() {
-    const intervalPicId = setTimeout(changeProfilePic, picDelay);
-    const intervalNameId = setTimeout(changeProfileName, nameDelay);
-    const timeOutID = setTimeout(changeDescription, descDelay);
-    const intervalCardId = setInterval(randomBackgroundColors, cardDelay);
-    if (picDelay === 0 && nameDelay === 0 && descDelay === 0) {
-        let userName = prompt("Enter first and last name");
-        profileName.value = userName;
+    function cardDOMManipulation() {
+        const intervalPicId = setTimeout(changeProfilePic, picDelay);
+        const intervalNameId = setTimeout(changeProfileName, nameDelay);
+        const timeOutID = setTimeout(changeDescription, descDelay);
+        const intervalCardId = setInterval(randomBackgroundColors, cardDelay);
+        setTimeout(userChangeName, nameChange);
     }
 
-}
+    function changeProfilePic() {
+        profilePic.src = "https://images.pexels.com/photos/4668513/pexels-photo-4668513.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
+    }
 
-function changeProfilePic () {
-    profilePic.src = "https://images.pexels.com/photos/4668513/pexels-photo-4668513.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
-}
+    function changeProfileName() {
+        profileName.innerHTML = "Jane Doe";
+    }
 
-function changeProfileName() {
-    profileName.innerHTML = "Jane Doe";
-}
+    function changeDescription() {
+        profileDesc.classList.add('desc-style');
+    }
 
-function changeDescription() {
-    profileDesc.classList.add('desc-style');
-}
-
-function changeCardBackground() {
-    profileCard.classList.toggle('card-background');
-}
+    function changeCardBackground() {
+        profileCard.classList.toggle('card-background');
+    }
 
 // BONUS
-function randomBackgroundColors(){
-    profileCard.style.backgroundColor = colorsArr[Math.floor(Math.random() * colorsArr.length)];
-}
+    function randomBackgroundColors() {
+        profileCard.style.backgroundColor = colorsArr[Math.floor(Math.random() * colorsArr.length)];
+    }
 
-cardDOMManipulation();
+    function userChangeName() {
+        profileName.innerHTML = prompt("Enter first and last name");
+    }
 
+    cardDOMManipulation();
 
-
-
-
-// use set timeout
-
-// Write code that toggles a class on the "profile-card" that changes its background color every two seconds. Use setInterval
-
+})();
